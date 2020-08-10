e657c1f31140b0f87a865001da26eb6089668f62
SamuelFajuyi <60722989+SamuelFajuyi@users.noreply.github.com>
2020-08-10 16:16:12 -0400

    updated journal

--- Journal-App/src/main/java/jar/Journal.java
+++ Journal-App/src/main/java/jar/Journal.java
@@ -1,9 +1,26 @@
 package jar;
 
+import java.util.Scanner;
+
 public class Journal {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scan = new Scanner(System.in);
+
+        UserM note = new UserM();
+        String menu = "0";
+
+        while (!menu.equals("4"))
+        {
+            System.out.println("what are we doing today? ");
+            System.out.println("1 - Add an entry");
+            System.out.println("2 - Search for entries");
+            System.out.println("3 - Delete entries");
+            System.out.println("4 - BYE");
+            menu = scan.nextLine();
+            System.out.println();
+
+        }
     }
     
 }
